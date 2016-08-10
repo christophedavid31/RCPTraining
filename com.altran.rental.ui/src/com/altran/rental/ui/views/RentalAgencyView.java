34b383527165831751cb6ca97c8e0eb9951c059c
cdavid <christophe.david@altran.com>
2016-08-10 14:22:29 +0200

    Initiated TreeViewer
    Signed-off-by: cdavid <christophe.david@altran.com>

---
+++ com.altran.rental.ui/src/com/altran/rental/ui/views/RentalAgencyView.java
@@ -0,0 +1,46 @@
+ 
+package com.altran.rental.ui.views;
+
+import javax.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+import com.altran.rental.ui.RentalProvider;
+import com.opcoach.training.rental.RentalAgency;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.TreeViewer;
+
+public class RentalAgencyView {
+	@Inject
+	public RentalAgencyView() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent, RentalAgency a) {
+		TreeViewer tv = new TreeViewer(parent);
+		
+		Collection<RentalAgency> agencies = new ArrayList<>();
+		agencies.add(a);
+		
+		RentalProvider p = new RentalProvider();
+		tv.setContentProvider(p);
+		tv.setLabelProvider(p);
+		
+		tv.setInput(agencies);
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		
+	}
+	
+	
+}
