2970ca9fb47c8093991d451ea3f5f173153921b2
cdavid <christophe.david@altran.com>
2016-08-10 19:15:35 +0200

    Tried to add value in OSGi context (but does not work for now)
    Signed-off-by: cdavid <christophe.david@altran.com>

--- com.altran.rental.core/src/com/altran/rental/core/RentalCoreActivator.java
+++ com.altran.rental.core/src/com/altran/rental/core/RentalCoreActivator.java
@@ -1,20 +1,18 @@
 package com.altran.rental.core;
 
+import javax.inject.Inject;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.core.contexts.EclipseContextFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
-import com.opcoach.training.rental.RentalAgency;
-import com.opcoach.training.rental.core.helpers.RentalAgencyGenerator;
 
 public class RentalCoreActivator implements BundleActivator {
 
 	private static BundleContext context;
-	private static RentalAgency a = RentalAgencyGenerator.createSampleAgency();
-	
-	public static RentalAgency getAgency() {
-		return a;
-	}
-	
+		
 	static BundleContext getContext() {
 		return context;
 	}
@@ -23,8 +21,18 @@ public class RentalCoreActivator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
+	@Inject
 	public void start(BundleContext bundleContext) throws Exception {
 		RentalCoreActivator.context = bundleContext;
+		
+		// Recup code p108
+		Bundle e4Bundle = Platform.getBundle("org.eclipse.e4.ui.workbench");
+		if (e4Bundle != null)
+		{
+			BundleContext e4BundleContext = e4Bundle.getBundleContext();
+			IEclipseContext osgiCtx = EclipseContextFactory.getServiceContext(e4BundleContext);
+			osgiCtx.set("myOSGiKey","Hello OSGi");
+		}
 	}
 
 	/*
