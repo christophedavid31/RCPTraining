6f7a28fff32d59c06ee515deafff908470e402f0
Christophe DAVID <tof.david@gmail.com>
2016-08-12 12:49:42 +0200

    Set default palette in defaults
    Signed-off-by: Christophe DAVID <tof.david@gmail.com>

--- com.altran.rental.ui/src/com/altran/rental/ui/prefs/RentalAbstractPreferenceInitializer.java
+++ com.altran.rental.ui/src/com/altran/rental/ui/prefs/RentalAbstractPreferenceInitializer.java
@@ -21,6 +21,8 @@ public class RentalAbstractPreferenceInitializer extends AbstractPreferenceIniti
 		
 		store = new ScopedPreferenceStore(InstanceScope.INSTANCE, PLUGIN_ID);
 		
+		store.setDefault(PREF_PALETTE, "com.altran.rental.ui.palette1");
+		
 		store.setDefault(PREF_CUSTOMER_COLOR, StringConverter.asString(new RGB(255,0,0)));
 		store.setDefault(PREF_RENTAL_COLOR, StringConverter.asString(new RGB(0,0,255)));
 		store.setDefault(PREF_RENTAL_OBJECT_COLOR, StringConverter.asString(new RGB(0,255,0)));
